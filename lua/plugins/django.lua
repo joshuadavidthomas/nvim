c332a01778e6812a93a714538bc01fa94d2106a6
Josh <josh@joshthomas.dev>
2024-04-04 09:20:23 -0500

    disable development django-lsp for now

--- lua/plugins/django.lua
+++ lua/plugins/django.lua
@@ -23,40 +23,40 @@ return {
       },
     },
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        django_lsp = {},
-      },
-      setup = {
-        django_lsp = function(_, opts)
-          local configs = require("lspconfig.configs")
-          local util = require("lspconfig.util")
-
-          local root_files = {
-            "manage.py",
-            "pyproject.toml",
-          }
-
-          if not configs.django_lsp then
-            configs.django_lsp = {
-              default_config = {
-                -- cmd = { "lsp-devtools", "agent", "--", "django-lsp" },
-                cmd = vim.lsp.rpc.connect("127.0.0.1", 9000),
-                filetypes = { "htmldjango" },
-                root_dir = function(fname)
-                  return util.root_pattern(unpack(root_files))(fname) or util.path.dirname(fname)
-                end,
-                name = "django_lsp",
-                options = {
-                  logfile = util.path.join(vim.fn.stdpath("cache"), "django-lsp.log"),
-                },
-              },
-            }
-          end
-        end,
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       django_lsp = {},
+  --     },
+  --     setup = {
+  --       django_lsp = function(_, opts)
+  --         local configs = require("lspconfig.configs")
+  --         local util = require("lspconfig.util")
+  --
+  --         local root_files = {
+  --           "manage.py",
+  --           "pyproject.toml",
+  --         }
+  --
+  --         if not configs.django_lsp then
+  --           configs.django_lsp = {
+  --             default_config = {
+  --               -- cmd = { "lsp-devtools", "agent", "--", "django-lsp" },
+  --               cmd = vim.lsp.rpc.connect("127.0.0.1", 9000),
+  --               filetypes = { "htmldjango" },
+  --               root_dir = function(fname)
+  --                 return util.root_pattern(unpack(root_files))(fname) or util.path.dirname(fname)
+  --               end,
+  --               name = "django_lsp",
+  --               options = {
+  --                 logfile = util.path.join(vim.fn.stdpath("cache"), "django-lsp.log"),
+  --               },
+  --             },
+  --           }
+  --         end
+  --       end,
+  --     },
+  --   },
+  -- },
 }
