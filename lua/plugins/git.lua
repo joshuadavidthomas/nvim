55a126d5986fd6eb29a07eaa8dead00fa20687a2
Josh <josh@joshthomas.dev>
2024-05-19 12:11:04 -0500

    neovim updates
    - add diffview plugin
    - patch treesitter textobjects for now
    - update lazyvim
    - simplify neovim python project
    - add requirements

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,4 +12,17 @@ return {
       { "<leader>gi", "<CMD>Octo issue list<CR>", desc = "list repo [i]ssues" },
     },
   },
+  {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+    },
+    opts = {},
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" },
+    },
+  },
 }
