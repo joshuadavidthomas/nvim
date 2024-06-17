6cd6b9003a2dd36928367ed98e693c019975819d
Josh <josh@joshthomas.dev>
2024-06-17 15:48:09 -0500

    fix zenmode

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -11,13 +11,10 @@ return {
       end
     end,
   },
-  {
-    "folke/twilight.nvim",
-    cmd = "ZenMode",
-  },
+  "folke/twilight.nvim",
   {
     "folke/zen-mode.nvim",
-    cmd = "VeryLazy",
+    cmd = "ZenMode",
     opts = {
       plugins = {
         wezterm = {
@@ -25,20 +22,6 @@ return {
           font = "+4",
         },
       },
-      on_open = function(_)
-        vim.o.laststatus = 0
-        if vim.env.TMUX then
-          vim.fn.system([[tmux set status off]])
-          vim.fn.system([[tmux list-panes -F '\#F' | grep -q Z || tmux resize-pane -Z]])
-        end
-      end,
-      on_close = function(_)
-        vim.o.laststatus = 3
-        if vim.env.TMUX then
-          vim.fn.system([[tmux set status on]])
-          vim.fn.system([[tmux list-panes -F '\#F' | grep -q Z && tmux resize-pane -Z]])
-        end
-      end,
     },
     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
   },
