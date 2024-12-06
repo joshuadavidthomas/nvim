08aee54fe9042ceb87feebbafee6c763b502d935
Josh <josh@joshthomas.dev>
2024-12-06 09:32:55 -0600

    remove debug print

--- init.lua
+++ init.lua
@@ -11,11 +11,3 @@ vim.print = _G.dd
 
 require("config.lazy")
 require("josh").setup()
-
-if vim.fn.argc() > 0 and vim.fn.isdirectory(vim.fn.argv()[1]) == 1 then
-  -- Neovim was opened with a directory
-  vim.print("Opened to directory: " .. vim.fn.argv()[1])
-else
-  -- Neovim was opened without a directory
-  vim.print("Opened without directory")
-end
