518b27cae28145d2356bbc8f68545f2bf18da820
Josh <josh@joshthomas.dev>
2024-12-28 09:09:45 -0600

    add transparency to neovim config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -27,4 +27,14 @@ return {
     },
     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
   },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 }
