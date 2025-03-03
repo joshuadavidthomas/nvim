b53bf507d4d93503d2c53a93293b584d2db52103
Josh <josh@joshthomas.dev>
2025-03-03 08:42:37 -0600

    add jump labels and toggle off when searching

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,11 +2,27 @@ return {
   "folke/flash.nvim",
   event = "LazyFile",
   ---@type Flash.Config
-  opts = {},
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    modes = {
+      char = {
+        jump_labels = true,
+      },
+    },
+  },
+  config = function()
+    -- always set toggle false when leaving command mode, which allows
+    -- for toggling flash on when in search mode, but not persisting it
+    vim.api.nvim_create_autocmd("CmdlineLeave", {
+      callback = function()
+        require("flash").toggle(false)
+      end,
+    })
+  end,
   -- stylua: ignore
   keys = {
-    { ",s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { ",S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
