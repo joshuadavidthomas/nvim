16020eb762582707f13620d8d2ec0e5c7bbaa28f
Josh <josh@joshthomas.dev>
2025-04-16 09:21:34 -0500

    change python3 prog

--- lua/globals.lua
+++ lua/globals.lua
@@ -2,6 +2,6 @@ local p = require("utils.path")
 
 -- language setup
 vim.g.node_host_prog = p.platformdirs("mise").user_data_dir .. "/installs/npm-neovim/latest/bin/neovim-node-host"
-vim.g.python3_host_prog = p.platformdirs("nvim.new").user_config_dir .. "/.venv/bin/python"
+vim.g.python3_host_prog = vim.fn.stdpath("config") .. "/.venv/bin/python"
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
