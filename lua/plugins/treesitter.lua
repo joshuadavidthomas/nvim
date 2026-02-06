b74358224b99462300204ca49cfb1aa7456d7f04
Josh Thomas <josh@joshthomas.dev>
2026-02-06 22:02:16 +0000

    remove query predicates from treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,13 +8,12 @@ return {
     event = "LazyFile",
     lazy = require("utils.vim").opened_to_file, -- load treesitter early when opening a file from the cmdline
     init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- PERF: add nvim-treesitter queries to the rtp early
       -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
       -- no longer trigger the **nvim-treesitter** module to be loaded in time.
       -- Luckily, the only things that those plugins need are the custom queries, which we make available
       -- during startup.
       require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
     end,
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
